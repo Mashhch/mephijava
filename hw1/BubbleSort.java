3c786a24bb50f90a676f70e093010cb41781712f
Чернова Мария <mashhch@gmail.com>
2024-11-02 12:41:26 +0300

    hw1

---
+++ hw1/BubbleSort.java
@@ -0,0 +1,46 @@
+package hw1;
+
+public class BubbleSort {
+
+    static void BubbleSort(double[] arr) {
+        int i = 0, n = arr.length;
+
+        boolean swapNeeded = true;
+        while (i < n - 1 && swapNeeded) {
+            swapNeeded = false;
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j - 1] > arr[j]) {
+
+                    double temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                    swapNeeded = true;
+                }
+            }
+            if (!swapNeeded)
+                break;
+            i++;
+        }
+    }
+
+    static void BubbleSortDescending(double[] arr) {
+        int i = 0, n = arr.length;
+
+        boolean swapNeeded = true;
+        while (i < n - 1 && swapNeeded) {
+            swapNeeded = false;
+            for (int j = 1; j < n - i; j++) {
+                if (arr[j - 1] < arr[j]) {
+
+                    double temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                    swapNeeded = true;
+                }
+            }
+            if (!swapNeeded)
+                break;
+            i++;
+        }
+    }
+}
---
+++ hw1/RadixSort.java
@@ -0,0 +1,100 @@
+package hw1;
+
+import java.util.Arrays;
+
+public class RadixSort {
+
+    public static int[] sort(int[] numbers) {
+        int maximumNumber = findMaximumNumberIn(numbers);
+
+        int numberOfDigits = calculateNumberOfDigitsIn(maximumNumber);
+
+        int placeValue = 1;
+
+        while (numberOfDigits-- > 0) {
+            applyCountingSortOn(numbers, placeValue);
+            placeValue *= 10;
+        }
+
+        return numbers;
+    }
+
+    public static int[] sortDescending (int[] numbers) {
+        int maximumNumber = findMaximumNumberIn(numbers);
+
+        int numberOfDigits = calculateNumberOfDigitsIn(maximumNumber);
+
+        int placeValue = 1;
+
+        while (numberOfDigits-- > 0) {
+            applyCountingSortOnDescending(numbers, placeValue);
+            placeValue *= 10;
+        }
+
+        return numbers;
+    }
+
+    private static void applyCountingSortOn(int[] numbers, int placeValue) {
+        int range = 10; // radix or the base
+
+        int length = numbers.length;
+        int[] frequency = new int[range];
+        int[] sortedValues = new int[length];
+
+        for (int number : numbers) {
+            int digit = (number / placeValue) % range;
+            frequency[digit]++;
+        }
+
+        for (int i = 1; i < range; i++) {
+            frequency[i] += frequency[i - 1];
+        }
+
+        for (int i = length - 1; i >= 0; i--) {
+            int digit = (numbers[i] / placeValue) % range;
+            sortedValues[frequency[digit] - 1] = numbers[i];
+            frequency[digit]--;
+        }
+
+        System.arraycopy(sortedValues, 0, numbers, 0, length);
+    }
+
+    private static void applyCountingSortOnDescending (int[] numbers, int placeValue) {
+        int range = 10; // radix or the base
+
+        int length = numbers.length;
+        int[] frequency = new int[range];
+        int[] sortedValues = new int[length];
+
+        for (int number : numbers) {
+            int digit = (number / placeValue) % range;
+            frequency[digit]++;
+        }
+
+        for (int i = range - 2; i >= 0; i--) {
+            frequency[i] += frequency[i + 1];
+        }
+
+        for (int i = length - 1; i >= 0; i--) {
+            int digit = (numbers[i] / placeValue) % range;
+            sortedValues[frequency[digit] - 1] = numbers[i];
+            frequency[digit]--;
+        }
+
+        System.arraycopy(sortedValues, 0, numbers, 0, length);
+    }
+
+    private static int calculateNumberOfDigitsIn(int number) {
+        return (int) Math.log10(number) + 1; // valid only if number > 0
+    }
+
+    private static int findMaximumNumberIn(int[] arr) {
+        return Arrays.stream(arr).max().getAsInt();
+    }
+
+//    public static void main(String[] args) {
+//        int[] numbers = {387, 468, 134, 123, 68, 221, 769, 37, 7};
+//        RadixSort.sort(numbers);
+//    }
+
+}
